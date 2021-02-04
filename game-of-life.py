d52c9184865a50f1766e9b630fd72e769bfaa590
Jaeho Lee <jaeho.lee@snu.ac.kr>
2021-02-04 14:57:34 +0000

    :sparkles: Improve performance and add CLI

--- game-of-life.py
+++ game-of-life.py
@@ -1,37 +1,97 @@
-import copy
 from typing import Callable, List, Tuple
+from functools import partial
 
 import numpy as np
 from PIL import Image
-from scipy import signal
 
 from util import timeit
+
+
+@timeit()
+def count_neighbors(grid: np.ndarray) -> np.ndarray:
+    #  neighbor_cnts[y, x] is the number of live neighbors of grid[y, x].
+    #  scipy.signal.convolve2d() can be used, but manually adding is faster than using convolution.
+    neighbor_cnts = np.empty(grid.shape, dtype="uint8")
+    # fmt:off
+    # Inner area
+    neighbor_cnts[1:-1, 1:-1] = (
+        grid[:-2, :-2]  # top-left
+        + grid[:-2, 1:-1]  # top
+        + grid[:-2, 2:]  # top right
+        + grid[1:-1, 2:]  # right
+        + grid[1:-1, :-2]  # left
+        + grid[2:, 2:]  # bottom-right
+        + grid[2:, 1:-1]  # bottom
+        + grid[2:, :-2]  # bottom-left
+    )
+    # Four corners
+    neighbor_cnts[0, 0] = (  # top-left
+        grid[0, 1]  # right
+        + grid[1, 1]  # bottom-right
+        + grid[1, 0]  # bottom
+    )
+    neighbor_cnts[0, -1] = (  # top-right
+        grid[1, -1]  # bottom
+        + grid[1, -2]  # bottom-left
+        + grid[0, -2]  # left
+    )
+    neighbor_cnts[-1, -1] = (  # bottom-right
+        grid[-1, -2]  # left
+        + grid[-2, -2]  # top-left
+        + grid[-2, -1]  # top
+    )
+    neighbor_cnts[-1, 0] = (  # bottom-left
+        grid[-2, 0]  # top
+        + grid[-2, 1]  # top-right
+        + grid[-1, 1]  # right
+    )
+    # Four edges
+    neighbor_cnts[0, 1:-1] = (  # top
+        grid[0, 2:]  # right
+        + grid[1, 2:]  # bottom-right
+        + grid[1, 1:-1]  # bottom
+        + grid[1, :-2]  # bottom-left
+        + grid[0, :-2]  # left
+    )
+    neighbor_cnts[1:-1, -1] = (  # right
+        grid[2:, -1]  # bottom
+        + grid[2:, -2]  # bottom-left
+        + grid[1:-1, -2]  # left
+        + grid[:-2, -2]  # top-left
+        + grid[:-2, -1]  # top
+    )
+    neighbor_cnts[-1, 1:-1] = (  # bottom
+        grid[-1, :-2]  # left
+        + grid[-2, :-2]  # top-left
+        + grid[-2, 1:-1]  # top
+        + grid[-2, 2:]  # top-right
+        + grid[-1, 2:]  # right
+    )
+    neighbor_cnts[1:-1, 0] = (  # left
+        grid[:-2, 0]  # top
+        + grid[:-2, 1]  # top-right
+        + grid[1:-1, 1]  # right
+        + grid[2:, 1]  # bottom-right
+        + grid[2:, 0]  # bottom
+    )
+    # fmt:on
+    return neighbor_cnts
 
 
 @timeit()
 def progress(grid: np.ndarray) -> None:
     """Progress grid to the next generation."""
 
-    # neighbor_cnts[y, x] is the number of live neighbors of grid[y, x].
-    neighbor_cnts = signal.convolve2d(
-        grid,
-        np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]], dtype="uint8"),
-        mode="same",
-    )
-    height, width = grid.shape
-    old_grid = copy.deepcopy(grid)
+    neighbor_cnts = count_neighbors(grid)
 
-    for y in range(height):
-        for x in range(width):
-            live_neighbors_cnt = neighbor_cnts[y, x]
-            alive = old_grid[y, x]
+    grid_v = grid.ravel()
+    neighbor_cnt_v = neighbor_cnts.ravel()
 
-            if alive and 2 <= live_neighbors_cnt <= 3:
-                continue  # grid[y, x] stays alive
-            elif not alive and live_neighbors_cnt == 3:
-                grid[y, x] = 1
-            else:
-                grid[y, x] = 0
+    birth_rule = (grid_v == 0) & (neighbor_cnt_v == 3)
+    survive_rule = (grid_v == 1) & ((neighbor_cnt_v == 2) | (neighbor_cnt_v == 3))
+
+    grid_v[...] = 0
+    grid_v[birth_rule | survive_rule] = 1
 
 
 @timeit()
@@ -75,10 +135,10 @@ def parse_grid(text: str, size: Tuple[int, int], live: str = "*") -> np.ndarray:
 
 
 @timeit()
-def add_grid_frame(grid: np.ndarray, generation: int) -> None:
+def add_grid_frame(grid: np.ndarray, generation: int, pixels_per_cell: int) -> None:
     """Add the grid to the grid_frames"""
 
-    arr_grid = enlarge_image(grid * 255, PIXELS_PER_CELL)
+    arr_grid = enlarge_image(grid * 255, pixels_per_cell)
     image = Image.fromarray(arr_grid, mode="L").convert("P")
     grid_frames.append(image)
 
@@ -101,25 +161,12 @@ def save_frames(grid_frames: List[Image.Image], filename: str) -> None:
     )
 
 
-if __name__ == "__main__":
-    import argparse
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-v", "--verbose", action="store_true")
-    args = parser.parse_args()
-    if args.verbose:
-        timeit.on = True
-    else:
-        timeit.on = False
-
-    WIDTH = 60
-    HEIGHT = 40
-    MAX_GEN = 300
-    PIXELS_PER_CELL = 10
-    DURATION = 50
-
-    glider_gun = parse_grid(
-        """\
+def get_demo(name: str, size: Tuple[int, int]) -> np.ndarray:
+    if name == "random":
+        return np.random.randint(0, 2, size, dtype="uint8")
+    demos = {
+        "glidergun": lambda: parse_grid(
+            """\
     ........................O
     ......................O.O
     ............OO......OO............OO
@@ -130,13 +177,67 @@ if __name__ == "__main__":
     ...........O...O
     ............OO
     """,
-        size=(WIDTH, HEIGHT),
-        live="O",
-    )
+            size=size,
+            live="O",
+        ),
+    }
 
+    return demos[name]()
+
+
+if __name__ == "__main__":
+    # Setup command line options
+    import argparse
+
+    parser = argparse.ArgumentParser()
+
+    input_group = parser.add_mutually_exclusive_group(required=True)
+    input_group.add_argument("-i", "--in")
+    input_group.add_argument("-d", "--demo")
+
+    output_group = parser.add_mutually_exclusive_group()
+    output_group.add_argument("-o", "--out", default="out.gif")
+    output_group.add_argument("--debug-print", action="store_true")
+
+    size_group = parser.add_argument_group()
+    size_group.add_argument("-W", "--width", type=int, default=100)
+    size_group.add_argument("-H", "--height", type=int, default=100)
+
+    option_group = parser.add_argument_group()
+    option_group.add_argument("-M", "--max-gen", type=int, default=300)
+    option_group.add_argument("--ppc", type=int, default=1)
+
+    dev_group = parser.add_argument_group()
+    dev_group.add_argument("-p", "--profile", action="store_true")
+
+    args = parser.parse_args()
+
+    filename = args.out
+    size = (args.width, args.height)
+    max_gen = args.max_gen
+    ppc = args.ppc
+    DURATION = 50
+
+    timeit.on = args.profile
+
+    if getattr(args, "in"):
+        grid = None
+        raise NotImplementedError()
+    else:  # demo mode
+        grid = get_demo(args.demo, size)
+
+    # Run Game of Life
     grid_frames: List[Image.Image] = []
-    driver(glider_gun, handler=add_grid_frame, max_gen=MAX_GEN)
-    save_frames(grid_frames, "glider_gun.gif")
+
+    if args.debug_print:
+        driver(grid, handler=grid_print, max_gen=max_gen)
+    else:
+        driver(
+            grid,
+            handler=partial(add_grid_frame, pixels_per_cell=ppc),
+            max_gen=max_gen,
+        )
+        save_frames(grid_frames, filename)
 
     if timeit.on:
         print(timeit.records)
